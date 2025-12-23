21fca7ef0d3509cdf2bddb06d4a0c86446333547
Andrei Bigiu <abigiu@NLCQJP4F17V6.nl.kworld.kpmg.com>
2025-12-23 11:36:20 +0100

    Trying to fix images on deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'prajiturigeta.org',
+    site: 'https://prajiturigeta.org',
     base: '/',
 });
